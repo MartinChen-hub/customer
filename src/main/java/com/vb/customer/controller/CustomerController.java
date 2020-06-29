03c9133dedbe012661240111cd45dd82320124ed
markchen20190628 <1004185602@qq.com>
2020-06-29 11:24:39 +0800

    Initial commit

---
+++ src/main/java/com/vb/customer/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.vb.customer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/cu-test")
+public class CustomerController {
+
+    private static Map<String, String> customers = new HashMap<>();
+
+    static{
+        customers.put("1", "我");
+        customers.put("2", "父亲");
+        customers.put("3", "母亲");
+    }
+
+    @GetMapping(path = "/customers/{customerId}")
+    public String getCustomerName(String customerId){
+        log.info("receive a request to customer-srv to get customer name");
+        String customerName = customers.get(customerId);
+        if(StringUtils.isBlank(customerName)){
+            customerName = "customer not found";
+        }
+        return customerName;
+    }
+}
